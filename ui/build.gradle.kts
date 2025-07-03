0ab019efada0a85541ae01f105c6ba16f93e3d08
EREN ÇOLAK <erencol@hotmail.com>
2025-07-03 19:51:39 +0300

    Initial commit

---
+++ ui/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.mobizoe.ui"
+    compileSdk = 36
+
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("release") {
+                    from(components["release"])
+
+                    groupId = "com.mobizoe.ui"
+                    artifactId = "v"
+                    version = "1.0"
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+}
+
+
+
---
+++ ui/src/main/java/com/mobizoe/ui/TestClass.kt
@@ -0,0 +1,7 @@
+package com.mobizoe.ui
+
+class TestClass {
+    fun testFunction(){
+        print("Hello")
+    }
+}
